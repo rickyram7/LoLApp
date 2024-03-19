6c918e78e063ac7893803bbaa49b45cbee92f9c3
rickyram7 <ricky.ram7@gmail.com>
2024-03-19 14:54:50 -0500

    new changes

--- leaguesearch/src/App.js
+++ leaguesearch/src/App.js
@@ -8,16 +8,20 @@ function App() {
   const [playerData, setPlayerData] = useState({});
   const [matchIDs, setMatchIDs] = useState([]);
   const [participantsData, setParticipantsData] = useState([]);
-  const [winningTeam, setWinningTeam] = useState(null);
+  const [winningTeam, setWinningTeam] = useState([]);
+  const [currentMatchIndex, setCurrentMatchIndex] = useState(0);
+  
 
-  const API_KEY = "RGAPI-4f4f79ac-8e2f-4401-8ee5-86f2a627cea4";
+
+  const API_KEY = "RGAPI-7f12b03e-9d8a-4705-b834-c852d8f98e02";
+
+
 
   useEffect(() => {
     if (matchIDs.length > 0) {
-      const latestMatchID = matchIDs[0];
-      searchMatchInfo(latestMatchID);
+      searchMatchInfo(matchIDs[currentMatchIndex]);
     }
-  }, [matchIDs]);
+  }, [matchIDs, currentMatchIndex]);
 
   function searchForPlayer(event) {
     // Set up the API call
@@ -41,6 +45,7 @@ function App() {
     axios.get(APICallString).then(function (response) {
       console.log(response.data);
       setMatchIDs(response.data);
+      setCurrentMatchIndex(0);
     }).catch(function (error) {
       console.log(error);
     });
@@ -54,7 +59,7 @@ function App() {
     axios.get(APICallString).then(function (response) {
       console.log(response.data);
       setParticipantsData(response.data.info.participants);
-      setWinningTeam(response.data.info.teams.find(team => team.win === "Win").teamId);
+      setWinningTeam(response.data.info.teams);
     }).catch(function (error) {
       console.log(error);
     });
@@ -74,64 +79,70 @@ function App() {
 
     return teams;
   }
+  function handleNextMatch() {
+    setCurrentMatchIndex(prevIndex => prevIndex + 1);
+  }
 
   return (
     <div className="App">
       <div className="container">
-        <h1>League Summoner Search</h1>
+        <h1 className='white-color'>League of Legends Search</h1>
         <input type="text" onChange={e => setSearchText(e.target.value)} />
         <button onClick={e => searchForPlayer(e)}>Search for Summoner</button>
       </div>
 
       {JSON.stringify(playerData) !== '{}' ? <>
-        <p>{playerData.name}</p>
+        <p className='white-color'>{playerData.name}</p>
         <img width='250' height='250' src={'http://ddragon.leagueoflegends.com/cdn/14.5.1/img/profileicon/' + playerData.profileIconId + '.png'} alt="Summoner Icon Unavailable"></img>
-        <p>Summoner level {playerData.summonerLevel}</p>
+        <h3 className='white-color'>Summoner level: {playerData.summonerLevel}</h3>
       </>
         :
         <p>No player Data</p>
       }
-      {winningTeam !== null && (
-        <div>
-          <h1>{winningTeam === 100 ? 'Team 1 Victory' : 'Team 2 Victory'}</h1>
-        </div>
-      )}
+      
       {participantsData.length > 0 ? (
         <div>
-          <h1>Recent Match</h1>
+          <h1 className='white-color'>Match History</h1>
+          <h2 className='white-color'>Winning Team: {winningTeam[0].win === "Win" ? "Team 2" : "Team 1"}</h2>
           {Object.entries(groupParticipantsByTeam(participantsData)).map(([team, participants]) => (
             <div key={team}>
-              <h1>{team}</h1>
+              <h1 className='white-color'>{team}</h1>
               {participants.map((participant, index) => (
                 <div key={index}>
-                  <h1>Summoner Name: {participant.summonerName}</h1>
+                  <h2 className='white-color'>Summoner Name: {participant.summonerName}</h2>
                   <img width='200' height='200' src={'http://ddragon.leagueoflegends.com/cdn/14.5.1/img/profileicon/' + participant.profileIcon + '.png'} alt="Icon Unavailable"></img>
-                  <h4>Champion: {participant.championName} </h4>
-                  <img width='100' height='100' src={'http://ddragon.leagueoflegends.com/cdn/14.5.1/img/champion/' + participant.championName + '.png'} alt="Champion Unavailable"></img>
-                  <p>Level: {participant.champLevel} </p>
-                  <p>Kills: {participant.kills} </p>
-                  <p>Deaths: {participant.deaths} </p>
-                  <p>Assists: {participant.assists} </p>
-                  <p>Gold Earned: {participant.goldEarned} </p>
-                  <img width='75' height='75' src={'http://ddragon.leagueoflegends.com/cdn/14.5.1/img/item/' + participant.item0 + '.png'} alt="Items"></img>
-                  <img width='75' height='75' src={'http://ddragon.leagueoflegends.com/cdn/14.5.1/img/item/' + participant.item1 + '.png'} alt="Items"></img>
-                  <img width='75' height='75' src={'http://ddragon.leagueoflegends.com/cdn/14.5.1/img/item/' + participant.item2 + '.png'} alt="Items"></img>
-                  <img width='75' height='75' src={'http://ddragon.leagueoflegends.com/cdn/14.5.1/img/item/' + participant.item3 + '.png'} alt="Items"></img>
-                  <img width='75' height='75' src={'http://ddragon.leagueoflegends.com/cdn/14.5.1/img/item/' + participant.item4 + '.png'} alt="Items"></img>
-                  <img width='75' height='75' src={'http://ddragon.leagueoflegends.com/cdn/14.5.1/img/item/' + participant.item5 + '.png'} alt="Items"></img>
-                  <img width='75' height='75' src={'http://ddragon.leagueoflegends.com/cdn/14.5.1/img/item/' + participant.item6 + '.png'}></img>
-                  <p> Rank: </p>
-                  <img width='75' height='75' src={'http://ddragon.leagueoflegends.com/cdn/14.5.1/img/rank' + participant.rank + '.png'} alt="Rank"></img>
-                  
+                  <h3 className='white-color'> Position: {participant.individualPosition} </h3>
+                  <h2 className='white-color'>Champion: {participant.championName} </h2>
+                  <img width='150' height='150' src={'http://ddragon.leagueoflegends.com/cdn/14.5.1/img/champion/' + participant.championName + '.png'} alt="Champion Unavailable"></img>
+                  <h3 className='white-color'> Level: {participant.champLevel} </h3>
+                  <h3 className='white-color'>Kills: {participant.kills} </h3>
+                  <h3 className='white-color'>Deaths: {participant.deaths} </h3>
+                  <h3 className='white-color'>Assists: {participant.assists} </h3>
+                  <h3 className='white-color'>Gold Earned: {participant.goldEarned} </h3>
+                  <h3 className='white-color'> Creep Score: {participant.totalMinionsKilled} </h3>
+                  <h3 className='white-color'> Vision Score: {participant.visionScore} </h3>
+                  <h3 className='white-color'> Total Damage to Enemy Champions: {participant.totalDamageDealtToChampions} </h3>
+                  <img width='75' height='75' src={'https://ddragon.leagueoflegends.com/cdn/14.6.1/img/item/' + participant.item0 + '.png'} alt="Items"></img>
+                  <img width='75' height='75' src={'https://ddragon.leagueoflegends.com/cdn/14.6.1/img/item/' + participant.item1 + '.png'} alt="Items"></img>
+                  <img width='75' height='75' src={'https://ddragon.leagueoflegends.com/cdn/14.6.1/img/item/' + participant.item2 + '.png'} alt="Items"></img>
+                  <img width='75' height='75' src={'https://ddragon.leagueoflegends.com/cdn/14.6.1/img/item/' + participant.item3 + '.png'} alt="Items"></img>
+                  <img width='75' height='75' src={'https://ddragon.leagueoflegends.com/cdn/14.6.1/img/item/' + participant.item4 + '.png'} alt="Items"></img>
+                  <img width='75' height='75' src={'https://ddragon.leagueoflegends.com/cdn/14.6.1/img/item/' + participant.item5 + '.png'} alt="Items"></img>
+                  <img width='75' height='75' src={'https://ddragon.leagueoflegends.com/cdn/14.6.1/img/item/' + participant.item6 + '.png'}></img>
+                  <p>.</p>
+                  <p> .   </p>
                 </div>
+
               ))}
             </div>
           ))}
+          {currentMatchIndex < matchIDs.length - 1 && (
+            <button onClick={handleNextMatch}>Next Match</button>
+          )}
         </div>
       ) : (
         <p>No participants data available</p>
       )}
-
     </div>
   );
 }
